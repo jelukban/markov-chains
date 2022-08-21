58b6b7dad488164174f54ef695bbbfaa1bf50519
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-20 17:29:27 -0700

    Added n input into parameters for make_chain and make_text

--- markov.py
+++ markov.py
@@ -14,7 +14,7 @@ def open_and_read_file(file_path = sys.argv[1]):
     return contents
 
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -40,52 +40,75 @@ def make_chains(text_string):
     """
 
     chains = {}
-    # [ 1,2,3,4,5]
+
     text_string = text_string.split()
 
     for i in range (len(text_string) - 2):
-        word1, word2, word3 = text_string[i], text_string[i + 1], text_string[i + 2]
-        temp_key = (word1, word2)
+        temp_key = []
 
+        # Append something to temp_key
+        for num in range(n):
+            try:
+                word = text_string[i + num]
+                temp_key.append(word)
+            except IndexError:
+                continue
+
+    # n = 3 (0, 1, 2)
+    # ['word0', 'word1', 'word2', 'word3', 'word4', 'word5']
+        temp_key = tuple(temp_key)
+        # Check if temp_key
         if temp_key not in chains:
-            chains[temp_key] = [word3]
+            chains[temp_key] = [text_string[n]]
         elif temp_key in chains:
-            chains[temp_key].append(word3)
+            chains[temp_key].append(text_string[n])
+
+        # word1, word2, word3 = text_string[i], text_string[i + 1], text_string[i + 2]
+        # temp_key = (word1, word2)
+
+        # if temp_key not in chains:
+        #     chains[temp_key] = [word3]
+        # elif temp_key in chains:
+        #     chains[temp_key].append(word3)
 
     return chains
 
+# print(make_chains(open_and_read_file(), 5))
 
-def make_text(chains):
+def make_text(chains, n):
     """Return text from chains."""
 
     text = []
 
     initial_key = choice(list(chains.keys()))
-    text.append(initial_key[0])
-    text.append(initial_key[1])
+    for word in initial_key:
+        text.append(word)
 
     next_word = choice(list(chains[initial_key]))
     text.append(next_word)
 
-    current_key = (text[-2], text[-1])
+    current_key = text[-n:]
+    current_key = tuple(current_key)
 
     while current_key in chains:
 
         next_word = choice(list(chains[current_key]))
         text.append(next_word)
 
-        current_key = (text[-2], text[-1])
+        current_key = text[-n:]
+        current_key = tuple(current_key)
 
     return ' '.join(text)
 
+print(make_text(make_chains(open_and_read_file(), 5), 5))
 
-# Open the file and turn it into one long string
-input_text = open_and_read_file()
+# # Open the file and turn it into one long string
+# input_text = open_and_read_file()
 
-# Get a Markov chain
-chains = make_chains(input_text)
+# # Get a Markov chain
+# chains = make_chains(input_text)
 
-# Produce random text
-random_text = make_text(chains)
+# # Produce random text
+# random_text = make_text(chains)
 
-print(random_text)
+# print(random_text)
